43735deac752db8d113e811a703da9b0a263b6bd
Mayank Mittal <61585566+mayanksmarty@users.noreply.github.com>
2022-11-21 22:35:44 +0530

    Update clean.sh

--- Scripts/clean.sh
+++ Scripts/clean.sh
@@ -1,1 +1,13 @@
-rm -rf /var/www/html/app
+FILE=/var/www/html/app/server.js
+if test -f "$FILE"; then
+   echo "$FILE exists"
+   cd /var/www/html/
+   pm2 stop --silent server.js
+   pm2 delete server.js
+   killall -9 node
+else 
+    echo "$FILE does not exist."
+fi
+cd /var/www/html/
+sudo rm -rf app
+sudo mkdir app
