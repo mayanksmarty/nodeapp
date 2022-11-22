9220801d533d9ad1b9930c8fed8c2ffe155ee182
Mayank Mittal <61585566+mayanksmarty@users.noreply.github.com>
2022-11-22 11:08:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ async function randomId() {
 }
 
 app.get("/test", (req, res) => {
-  res.send("<h1>It's working fine 🤗</h1>")
+  res.send("<h1>It's working fine ok right now 🤗</h1>")
 })
 
 app.get("/", async (req, res) => {
